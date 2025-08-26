2fc23f843c39e7f60d977e06b9552726fd923af1
Junha_Song <62208537+junha-research@users.noreply.github.com>
2025-08-26 13:47:57 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from dotenv import load_dotenv
 from langchain.chat_models import init_chat_model
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
-load_dotenv()
 
 #ChatOpenAI 초기화
 llm = init_chat_model("gpt-4o-mini", model_provider="openai")
@@ -20,3 +18,4 @@ output_parser = StrOutputParser()
 chain = prompt|llm|output_parser
 result = chain.invoke({"input":"hi"})
 print(result)
+
