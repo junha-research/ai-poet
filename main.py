0f0db9b818af53eac7e09fa73e95f57d29899f00
Junha_Song <62208537+junha-research@users.noreply.github.com>
2025-08-26 13:52:50 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from langchain.chat_models import init_chat_model
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
+#from dotenv import load_dotenv
+import streamlit as st
+#load_dotenv()
 
-#ChatOpenAI 초기화
-llm = init_chat_model("gpt-4o-mini", model_provider="openai")
+#ChatOpenAI
+llm = init_chat_model("gpt-4o-mini",model_provider="openai")
 
-#프롬프트 탬플릿 생성
-prompt = ChatPromptTemplate([
-    ("system","You are Jean-Jacques Rousseau."),
+#프롬프트 템플릿 생성
+prompt = ChatPromptTemplate.from_messages([
+    ("system","You are Jean-Jacques Rousseau"),
     ("user","{input}")
 ])
 
@@ -16,6 +19,19 @@ output_parser = StrOutputParser()
 
 #LLM 체인 구성
 chain = prompt|llm|output_parser
-result = chain.invoke({"input":"hi"})
-print(result)
 
+# content = "자연"
+# result = chain.invoke({"input": content+"에 대한 시를 써줘"})
+# print(result)
+
+#제목
+st.title('인공지능 시인')
+
+#시 주제 입력 필드
+content = st.text_input("시의 주제를 제시해주세요")
+st.write("시의 주제는",content)
+
+if st.button("시 작성 요청하기"):
+    with st.spinner('Wait for it...'):
+        result = chain.invoke({"input":content + "에 대한 시를 써줘"})
+        st.write(result)
